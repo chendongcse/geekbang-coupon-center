1907853e844873984112eafd5ea7c8f6e36a78d3
Vincent Yao <vincent.yao@alu.fudan.edu.cn>
2021-11-19 00:45:23 +0800

    fix dummyprocessor

--- 0-Spring Boot急速落地部分/coupon-calculation-serv/coupon-calculation-impl/src/main/java/com/geekbang/coupon/calculation/processor/impl/DummyProcessor.java
+++ 0-Spring Boot急速落地部分/coupon-calculation-serv/coupon-calculation-impl/src/main/java/com/geekbang/coupon/calculation/processor/impl/DummyProcessor.java
@@ -1,5 +1,6 @@
 package com.geekbang.coupon.calculation.processor.impl;
 
+import com.geekbang.coupon.calculation.api.beans.ShoppingCart;
 import com.geekbang.coupon.calculation.processor.AbstractRuleProcessor;
 import com.geekbang.coupon.calculation.processor.RuleProcessor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class DummyProcessor extends AbstractRuleProcessor implements RuleProcessor {
+
+    @Override
+    public ShoppingCart calculate(ShoppingCart order) {
+        // 获取订单总价
+        Long orderTotalAmount = getTotalPrice(order.getProducts());
+
+        order.setCost(orderTotalAmount);
+        return order;
+    }
+
 
     @Override
     protected Long calculateNewPrice(Long orderTotalAmount, Long shopTotalAmount, Long quota) {
