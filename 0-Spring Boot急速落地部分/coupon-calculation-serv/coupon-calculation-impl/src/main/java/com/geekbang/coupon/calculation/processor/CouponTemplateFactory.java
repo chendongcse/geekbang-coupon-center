e675b71aeac5ab4d99d71240fa779dabe05a29e2
Vincent Yao <vincent.yao@alu.fudan.edu.cn>
2021-11-20 00:48:37 +0800

    添加夜色撩人双倍券

--- 0-Spring Boot急速落地部分/coupon-calculation-serv/coupon-calculation-impl/src/main/java/com/geekbang/coupon/calculation/processor/CouponTemplateFactory.java
+++ 0-Spring Boot急速落地部分/coupon-calculation-serv/coupon-calculation-impl/src/main/java/com/geekbang/coupon/calculation/processor/CouponTemplateFactory.java
@@ -1,10 +1,7 @@
 package com.geekbang.coupon.calculation.processor;
 
 import com.geekbang.coupon.calculation.api.beans.ShoppingCart;
-import com.geekbang.coupon.calculation.processor.impl.DiscountTemplate;
-import com.geekbang.coupon.calculation.processor.impl.DummyTemplate;
-import com.geekbang.coupon.calculation.processor.impl.MoneyOffTemplate;
-import com.geekbang.coupon.calculation.processor.impl.RandomReductionTemplate;
+import com.geekbang.coupon.calculation.processor.impl.*;
 import com.geekbang.coupon.template.api.beans.CouponTemplateInfo;
 import com.geekbang.coupon.template.api.enums.CouponType;
 import lombok.extern.slf4j.Slf4j;
@@ -12,26 +9,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
+// 工厂方法根据订单中的优惠券信息，返回对应的Template用于计算优惠价
 @Component
 @Slf4j
 public class CouponTemplateFactory {
 
     @Autowired
-    private MoneyOffTemplate moneyOffProcessor;
+    private MoneyOffTemplate moneyOffTemplate;
 
     @Autowired
-    private DiscountTemplate discountProcessor;
+    private DiscountTemplate discountTemplate;
 
     @Autowired
-    private RandomReductionTemplate randomReductionProcessor;
+    private RandomReductionTemplate randomReductionTemplate;
 
     @Autowired
-    private DummyTemplate dummyProcessor;
+    private LonelyNightTemplate lonelyNightTemplate;
+
+    @Autowired
+    private DummyTemplate dummyTemplate;
 
     public RuleTemplate getTemplate(ShoppingCart order) {
         // 不使用优惠券
         if (CollectionUtils.isEmpty(order.getCouponInfos())) {
-            return dummyProcessor;
+            // dummy模板直接返回原价，不进行优惠计算
+            return dummyTemplate;
         }
 
         // 暂时不支持多张优惠券
@@ -45,15 +47,21 @@ public class CouponTemplateFactory {
         CouponType category = CouponType.convert(template.getType());
 
         switch (category) {
+            // 订单满减券
             case MONEY_OFF:
-                return moneyOffProcessor;
-            case DISCOUNT:
-                return discountProcessor;
+                return moneyOffTemplate;
+            // 随机立减券
             case RANDOM_DISCOUNT:
-                return randomReductionProcessor;
+                return randomReductionTemplate;
+            // 午夜下单优惠翻倍
+            case LONELY_NIGHT_MONEY_OFF:
+                return lonelyNightTemplate;
+            // 打折券
+            case DISCOUNT:
+                return discountTemplate;
             // 未知类型的券模板
             default:
-                return dummyProcessor;
+                return dummyTemplate;
         }
     }
 
--- 0-Spring Boot急速落地部分/coupon-template-serv/coupon-template-api/src/main/java/com/geekbang/coupon/template/api/enums/CouponType.java
+++ 0-Spring Boot急速落地部分/coupon-template-serv/coupon-template-api/src/main/java/com/geekbang/coupon/template/api/enums/CouponType.java
@@ -13,7 +13,8 @@ public enum CouponType {
     UNKNOWN("unknown", "0"),
     MONEY_OFF("满减券", "1"),
     DISCOUNT("打折", "2"),
-    RANDOM_DISCOUNT("随机减", "3");
+    RANDOM_DISCOUNT("随机减", "3"),
+    LONELY_NIGHT_MONEY_OFF("寂寞午夜double券", "4");
 
     private String description;
 
